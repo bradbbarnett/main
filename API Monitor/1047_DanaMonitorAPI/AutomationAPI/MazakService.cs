2142ab84bce30609ce31fee1929e84ba3b87fd80
bradbbarnett <bradbbarnett@gmail.com>
2024-01-04 08:46:44 -0500

    Windows Service

---
+++ API Monitor/1047_DanaMonitorAPI/AutomationAPI/MazakService.cs
@@ -0,0 +1,37 @@
+﻿using System.Timers;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AutomationAPI
+{
+    public partial class MazakService : ServiceBase
+    {
+        Thread mainThread;
+        Thread pingThread;
+
+        public MazakService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //Start Main Program Loop
+            mainThread = new Thread(new ThreadStart(MainRoutine.MainMethod));
+            mainThread.Start();
+
+            //Start ping thread
+            pingThread = new Thread(new ThreadStart(MainRoutine.Ping));
+            pingThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            //Disconnect communications
+            MainRoutine.Stop();
+            mainThread.Abort();
+            pingThread.Abort();
+        }
+    }
+}
